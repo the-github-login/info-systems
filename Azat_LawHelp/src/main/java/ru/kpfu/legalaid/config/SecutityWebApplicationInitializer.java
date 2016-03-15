654372053055a5e09bff69e459504c072c494bf8
Ruzalka <the-email-name@mail.ru>
2016-03-15 14:24:46 +0300

    test

--- Azat_LawHelp/src/main/java/ru/kpfu/legalaid/config/SecutityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package ru.kpfu.legalaid.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-/**
- * Created by Rail on 17.05.2015.
- */
-public class SecutityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
--- Azat_LawHelp/src/main/webapp/WEB-INF/jsp/access_denied.jsp
+++
@@ -1,1 +0,0 @@
-<h1>403. You don't have needed authorities to make this action.</h1>
